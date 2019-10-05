83a8e245083291611453af9241b50e96eb29479e
Benjamin Hoipkemier <benjamin.hoipkemier@maritzcx.com>
2019-10-05 09:35:59 -0400

    activate worker

--- nsUi/userscript.js
+++ nsUi/userscript.js
@@ -9,20 +9,24 @@
 // @require      https://code.jquery.com/jquery-1.12.4.min.js
 // ==/UserScript==
 
+// curl "https://mrbc.ccbchurch.com/api/scheduling/event_positions/4562/assignments" -H "sec-fetch-mode: cors" -H "origin: https://mrbc.ccbchurch.com" -H "accept-encoding: gzip, deflate, br" -H "accept-language: en-US,en;q=0.9,en-CA;q=0.8" -H "cookie: __cfduid=d0d3ef46962a5de6d9df822fcc28348891570280086; screen-size=SIZE/LARGE; sidebar-opened=true; _ga=GA1.2.903768189.1570280091; _gid=GA1.2.466744206.1570280091; PHPSESSID=8c0c3b1baff9c088eda6af2639f87b83; _uiq_id.603070201.d525=fe12c19898493989.1570280091.0.1570281160..; _gat=1" -H "pragma: no-cache" -H "x-newrelic-id: VQIGWVdRCxACUFhUBAcCUlA=" -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36" -H "content-type: application/json;charset=UTF-8" -H "accept: application/json, text/plain, */*" -H "cache-control: no-cache" -H "authority: mrbc.ccbchurch.com" -H "referer: https://mrbc.ccbchurch.com/goto/scheduling/14/grid?schedule_ids^[^]=105" -H "sec-fetch-site: same-origin" --data-binary "^{^\^"individual_ids^\^":^[2806^],^\^"status^\^":null,^\^"update_existing_status^\^":false,^\^"update_notified^\^":false,^\^"notify_schedule_organizer^\^":false^}" --compressed
+
 var curWorkers = [];
 var workerCount = {};
 (function() {
 	'use strict';
 	const css = `
 		.hoipUI { position: absolute; left: 0; right:0; top:0; }
-		.tblAvailability th { background-color: #f9f9f9; }
+		.tblAvailability { background-color: #fff; }
+		.tblAvailability th { text-align: center;background-color: #ccc;}
+		.tblAvailability td { vertical-align: text-top;}
+		.tblAvailability th,.tblAvailability td { border: 1px solid #000; }
 		.passign .badge { display: none }
 		.active.passign .badge { display: block }
 		.eventContainer { padding-bottom: 15px; }
 		.eventContainer.peopleReq, .eventContainer.unAvail { padding-bottom: 30px; cursor: pointer; }
 		.list-group.eventContainer { margin-bottom: 0 }
-		.peopleReq { background-color: rgb(214,233,198); border: 1px solid rgb(60,118,61); }
-		.unAvail { background-color: rgb(235,204,209); border: 1px solid rgb(169,68,66); }
+		.active.person, .avail { background-color: rgb(214,233,198); border: 1px solid rgb(60,118,61); }
 	`;
 	const log = (function(msg){ console.log(msg); });
 	const addMenuItem = (function(menu){
@@ -41,6 +45,25 @@ var workerCount = {};
 		$('head').append('<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous"><style>'+css+'</style>');
 		fetchScheduleData(getScheduleIds(), loadTable);
 		fetchNurseryWorkers();
+		addHoipEvents();
+	});
+
+	var addHoipEvents = (function() {
+		$('.hoipUI').on('click', '.people .person', activatePerson)
+			.on('click', '.addToEvent', addToEvent);
+	});
+
+	var addToEvent = (function() {
+		const eventid = $('.eventID', $(this).closest('td')).val();
+		const personid = $('.people .person.active .personID').val();
+		console.log('Add eventId: ' + eventid + ' personId: ' + personid);
+	});
+
+	var activatePerson = (function() {
+		$('.active').removeClass('active');
+		$(this).addClass('active');
+		const personId = $('.personID', this).val();
+		$('.tblAvailability .person input[value="'+personId+'"]').closest('.person').addClass('active');
 	});
 
 	var fetchNurseryWorkers = (function(){
@@ -73,7 +96,7 @@ var workerCount = {};
 	var workersLoaded = function() {
 		const workerDiv = $('<div class="list-group people col-md-2"/>')
 		curWorkers.forEach(w => {
-			const a = $('<a href="#" class="list-group-item person"/>').text(flipName(w.name));
+			const a = $('<div class="list-group-item person"/>').text(flipName(w.name));
 			a.append($('<input type="hidden" class="personID"/>').attr('value',w.id))
 			a.append($('<span class="occurCount badge">0</span>'));
 			workerDiv.append(a);
@@ -137,18 +160,20 @@ var workerCount = {};
 	var getEventRow = function(event){
 		const dateText = new Date(event.start).toLocaleDateString() + ' ' + new Date(event.start).toLocaleTimeString().replace(':00 ', ' ');
 		const toReturn = $('<tr/>')
-		toReturn.append($('<td/>').text(dateText));
+		toReturn.append($('<th/>').text(dateText));
 		getEventPositions(event).forEach(position => {
 			const eventContainer = $('<div class="eventContainer list-group"/>')
 			const cell = $('<td/>').append(eventContainer);
-		
+			cell.append($('<input type="hidden" class="eventID" />').val(position.id))
+
 			position.assignments.forEach(assignment => {
-				var passign = $('<a href="" class="passign person list-group-item"/>')
+				var passign = $('<a href="javascript:void" class="passign person list-group-item"/>')
 					.text(assignment.volunteer.individual.name)
-					.append('<a href="javascrip:void" class="btn badge"><i class="glyphicon glyphicon-trash removePassign"></i></a>')
+					.append('<a href="javascript:void" class="btn badge"><i class="glyphicon glyphicon-trash removePassign"></i></a>')
 					.append($('<input type="hidden" />').val(assignment.volunteer.individual.id));
 				eventContainer.append(passign);
 			});
+			cell.append('<button class="addToEvent">Add to Event</button');
 			toReturn.append(cell);
 		});
 		return toReturn;
